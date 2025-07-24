1d01344b91a240144f27d332e582558ca366c475
Gianni Bonicolini <g.bonicolini@sintraconsulting.eu>
2025-07-24 14:40:29 +0200

    Added exception handling on PLaywright initialization

--- src/PdfSmith/BackgroundServices/PlaywrightBackgroundService.cs
+++ src/PdfSmith/BackgroundServices/PlaywrightBackgroundService.cs
@@ -1,15 +1,32 @@
-﻿
+﻿using Microsoft.Playwright;
 using PdfSmith.HealthChecks;
 
 namespace PdfSmith.BackgroundServices;
 
-public class PlaywrightBackgroundService(PlaywrightHealthCheck playwrightHealthCheck) : BackgroundService
+public class PlaywrightBackgroundService(ILogger<PlaywrightBackgroundService> logger, PlaywrightHealthCheck playwrightHealthCheck) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         // On Windows, it is installed in %USERPROFILE%\AppData\Local\ms-playwright by default
         // We can use PLAYWRIGHT_BROWSERS_PATH environment variable to change the default location
-        await Task.Run(() => Microsoft.Playwright.Program.Main(["install", "chromium"]));
+        var (returnCode, errorMessage) = await Task.Run(() =>
+        {
+            try
+            {
+                return (Microsoft.Playwright.Program.Main(["install", "chromium"]), (string?)null);
+            }
+            catch (PlaywrightException playwrightException)
+            {
+                logger.LogError(playwrightException, "Error while installing chromium");
+                return (1, "Error while initializing Playwright");
+            }
+        });
+
+        if (returnCode != 0)
+        {
+            playwrightHealthCheck.ErrorMessage = errorMessage;
+            return;
+        }
 
         playwrightHealthCheck.IsPlaywrightReady = true;
     }
--- src/PdfSmith/HealthChecks/PlaywrightHealthCheck.cs
+++ src/PdfSmith/HealthChecks/PlaywrightHealthCheck.cs
@@ -5,6 +5,7 @@ namespace PdfSmith.HealthChecks;
 public class PlaywrightHealthCheck : IHealthCheck
 {
     public bool IsPlaywrightReady { get; set; } = false;
+    public string? ErrorMessage { get; set; }
 
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
@@ -13,6 +14,7 @@ public class PlaywrightHealthCheck : IHealthCheck
             return Task.FromResult(HealthCheckResult.Healthy());
         }
 
-        return Task.FromResult(HealthCheckResult.Unhealthy("Playwright isn't ready yet."));
+        var unhealthyMessage = ErrorMessage ?? "Playwright isn't ready yet.";
+        return Task.FromResult(HealthCheckResult.Unhealthy(unhealthyMessage));
     }
 }
